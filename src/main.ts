8929b01902a672460b42d05cb545ea8569408547
hayk98-hayrapetyan <hayk.hayrapetyan98@gmail.com>
2024-11-08 14:37:12 +0400

    add mdi icons package and use date input

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import './assets/main.css'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
+import { VDateInput } from 'vuetify/labs/VDateInput'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VDateInput,
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
